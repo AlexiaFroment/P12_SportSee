13c180da70ce9986a9479824974525f34adbe15a
AloaCmoi <aloacmoi@gmail.com>
2026-06-13 14:14:13 +0200

    update home url

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -9,7 +9,10 @@ export const NotFound: React.FC = () => {
       <p className='p-2  fs-2' style={{ color: "#ff0000" }}>
         Oups ! La page que vous demandez n'existe pas
       </p>
-      <a href='/accueil' className='p-2 fs-3 ' style={{ color: "#ff0000" }}>
+      <a
+        href='https://alexiafroment.github.io/'
+        className='p-2 fs-3 '
+        style={{ color: "#ff0000" }}>
         Retourner sur la page accueil
       </a>
     </section>
